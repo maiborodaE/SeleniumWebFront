a590eba753e88f970a5b8d97ef35107fb0ea7d16
maiboroda.e <evgeniy.maiboroda@gmail.com>
2018-02-22 19:46:08 +0200

    FF and manuf

---
+++ src/test/java/Manufacturers/ChromeManufTest.java
@@ -0,0 +1,13 @@
+package Manufacturers;
+
+import Credentials.UserCredentials;
+
+public class ChromeManufTest {
+    private static String randomName = UserCredentials.randomName;
+    private static String activityMenu = UserCredentials.activityMenu;
+    private static String constrAnketMenu = UserCredentials.constrAnketMenu;
+    private static String createQHbutton = UserCredentials.createQHbutton;
+    private static String saveQHbutton = UserCredentials.saveQHbutton;
+    private static String deleteQHbutton = UserCredentials.deleteQHbutton;
+
+}
